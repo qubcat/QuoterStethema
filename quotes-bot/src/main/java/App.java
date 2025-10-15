bb5fa1a5bf313005e8e5e64481e7824c86343bde
qubcat <qubcat@ya.ru>
2025-10-15 22:16:21 +0200

    Минимальный рабочий код

--- quotes-bot/src/main/java/App.java
+++ quotes-bot/src/main/java/App.java
@@ -1,2 +1,16 @@
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
 public class App {
+    public static void main(String[] args) throws Exception {
+        String token = System.getenv("BOT_TOKEN");
+        String username = System.getenv("BOT_USERNAME");
+        if (token == null || username == null) {
+            System.err.println("Set BOT_TOKEN and BOT_USERNAME env vars");
+            System.exit(1);
+        }
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(new MyQuoteBot(token, username));
+        System.out.println("Quotes bot started as @" + username);
+    }
 }
